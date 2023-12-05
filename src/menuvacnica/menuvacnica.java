b60398ca1304dd2d6281b714028c5d5713530329
DamjanKolevski <damjankolevski9@gamil.com>
2023-12-05 18:12:45 +0100

    first commit

---
+++ src/menuvacnica/menuvacnica.java
@@ -0,0 +1,13 @@
+package menuvacnica;
+
+public class menuvacnica {
+public static void main(String[] args) {
+	double EUR= 61.65;
+       int MKD= 72;
+       double suma= (EUR*MKD);
+       System.out.println (suma);
+
+
+	}
+
+}
